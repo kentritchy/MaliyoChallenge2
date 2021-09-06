2770b5f76d568bad41525940482972086f306a3e
kentritchy <83905595+kentritchy@users.noreply.github.com>
2021-09-06 05:08:15 +0200

    Add Challenge 2

---
+++ Dog-Baloon Challenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float timeAllowable = 0.6f;
+    private float nextSpawn;
+
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + timeAllowable;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# MaliyoChallenge2
- 
